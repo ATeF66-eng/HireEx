4c8c43792ded9e6bf2fd9c4a00efd9927ae5dce3
Abdulrhman1Hany <abdulrhmanhany@std.mans.edu.eg>
2025-04-25 18:50:59 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 int main(void) {
     printf("Welcome to Hirex\n the perfect place to find/offer a job\n");
 int x;
-    printf("choose\n 1-find a job\n 2-offer a job\n");
+    printf(" 1-find a job\n 2-offer a job\n");
     scanf("%d",&x);
     if(x==1) {
 printf("1-Engneering\n 2-Medical field\n 3-Arts\n 4-Business\n 5-Education");
@@ -14,7 +14,11 @@ printf("1-Engneering\n 2-Medical field\n 3-Arts\n 4-Business\n 5-Education");
         printf("Nice Job\n",y);
         if (y==1) {
             int z;
-            printf("1-Programming Engineer\n 2-Communication Engineer\n 3-IT Engineer\n 4-Architectural Engineer\n 5-Electric field\n");
+            printf("1-Programming Engineer | Full Time | Cairo | at Siemens digital industries \n
+                2-Communication Engineer | Part Time | Giza | at EgyptAir  Company \n
+                3-IT Engineer | Part Time | Remotly | at FAMA Company \n
+                4-Architecture Engineer| Full Time | Dubai | at Arabian Conctruction Company \n
+                5-Electric field | Internship | Cairo | at Elswedy Electric Company \n");
             scanf("%d",&z);
 
         }
