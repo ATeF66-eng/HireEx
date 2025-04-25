242e853fc59f7fe52c8afff5cfb547ff2eb0a178
ATeF66-eng <abdelrahmanelsabea@gmail.com>
2025-04-25 19:36:53 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,46 +6,45 @@
 
 int main(void) {
     printf("Welcome to Hirex\n the perfect place to find/offer a job\n");
-int x;
+    int x;
     printf("choose\n 1-find a job\n 2-offer a job\n");
     scanf("%d",&x);
     if(x==1) {
-printf("1-Engineering\n 2-Medical field\n 3-Arts\n 4-Business\n 5-Education\n");
-        int y;
-        scanf("%d",&y);
+        printf("1-Engineering\n 2-Medical field\n 3-Arts\n 4-Business\n 5-Education\n");
+        int field;
+        scanf("%d",&field);
         printf("Nice Job\n");
-        if (y==1) {
+        if (field==1) {
             int z=0;
-            printf("1-Programming Engineer | Full Time | Cairo | at Siemens digital industries \n2-Communication Engineer | Part Time | Giza | at EgyptAir  Company \n3-IT Engineer | Part Time | Remotly | at FAMA Company \n4-Architecture Engineer| Full Time | Dubai | at Arabian Conctruction Company \n5-Electric field | Internship | Cairo | at Elswedy Electric Company \n");
+            printf("1-Programming Engineer | Full Time | Cairo | at Siemens digital industries \n2-Communication Engineer | Part Time | Giza | at EgyptAir  Company \n3-IT Engineer | Part Time | Remotely | at FAMA Company \n4-Architecture Engineer| Full Time | Dubai | at Arabian Conctruction Company \n5-Electric field | Internship | Cairo | at Elswedy Electric Company \n");
             scanf("%d",&z);
-            if (z==1) {
-                switch (z) {
-                    case 1:
-                        printf("The Requirements for this job is :\n");
-                    break;
-                    case 2:
-                        printf("The requirements for this job is :\n");
-                    break;
-                    case 3:
-                        printf("The requirements for this job is :\n");
-                    break;
-                    case 4:
-                        printf("The requirements for this job is :\n");
-                    break;
-                    case 5:
-                        printf("The requirements for this job is :\n");
-                    break;
-                    default:
-                        printf("Error :\n");
-                }
-            }else
-                printf("Error :\n");
 
-    }
-    if (y ==2) {
-        printf("1-Cardiologist\n 2-Dentist\n 3-Surgeon\n 4-Veterinarian\n 5-");
+            switch (z) {
+                case 1:
+                    printf("The Requirements for this job are :\n");
+                break;
+                case 2:
+                    printf("The requirements for this job are :\n");
+                break;
+                case 3:
+                    printf("The requirements for this job are :\n");
+                break;
+                case 4:
+                    printf("The requirements for this job are :\n");
+                break;
+                case 5:
+                    printf("The requirements for this job are :\n");
+                break;
+                default:
+                    printf("Error :\n");
+            }
+
+
+            if (field ==2) {
+                printf("1-Cardiologist | full time | Cairo |at Clinca Egypt \n 2-Dentist | part time | Giza |at Al-Arabi clinics  \n 3-Surgeon | internship | Mansoura |at Technoclinc specialized clinics\n 4-Veterinarian |part time| Zifta |at Al Safa Veterinary clinic  \n 5-Allergist |full time| Cairo| at Core clinic|\n");
+            }
+        }
     }
 }
 
-        }
 
