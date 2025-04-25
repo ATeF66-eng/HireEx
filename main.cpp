f1e5fb73998fd78282c271e62351ec9cb5901b8c
ATeF66-eng <abdelrahmanelsabea@gmail.com>
2025-04-25 19:05:55 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,22 +5,43 @@
 int main(void) {
     printf("Welcome to Hirex\n the perfect place to find/offer a job\n");
 int x;
-    printf(" 1-find a job\n 2-offer a job\n");
+    printf("choose\n 1-find a job\n 2-offer a job\n");
     scanf("%d",&x);
     if(x==1) {
-printf("1-Engneering\n 2-Medical field\n 3-Arts\n 4-Business\n 5-Education");
+printf("1-Engineering\n 2-Medical field\n 3-Arts\n 4-Business\n 5-Education\n");
         int y;
         scanf("%d",&y);
-        printf("Nice Job\n",y);
+        printf("Nice Job\n");
         if (y==1) {
-            int z;
-            printf("1-Programming Engineer | Full Time | Cairo | at Siemens digital industries \n
-                2-Communication Engineer | Part Time | Giza | at EgyptAir  Company \n
-                3-IT Engineer | Part Time | Remotly | at FAMA Company \n
-                4-Architecture Engineer| Full Time | Dubai | at Arabian Conctruction Company \n
-                5-Electric field | Internship | Cairo | at Elswedy Electric Company \n");
+            int z=0;
+            printf("1-Programming Engineer | Full Time | Cairo | at Siemens digital industries \n2-Communication Engineer | Part Time | Giza | at EgyptAir  Company \n3-IT Engineer | Part Time | Remotly | at FAMA Company \n4-Architecture Engineer| Full Time | Dubai | at Arabian Conctruction Company \n5-Electric field | Internship | Cairo | at Elswedy Electric Company \n");
             scanf("%d",&z);
+switch (z) {
+    case 1:
+        printf("The Requirements for this job is :\n");
+    break;
+    case 2:
+        printf("The requirements for this job is :\n");
+    break;
+    case 3:
+        printf("The requirements for this job is :\n");
+    break;
+    case 4:
+        printf("The requirements for this job is :\n");
+    break;
+    case 5:
+        printf("The requirements for this job is :\n");
+    break;
+    default:
+printf("Error :\n");
+}
 
-        }
+    }
+    if (y ==2) {
+        printf("1-Cardiologist\n 2-Dentist\n 3-Surgeon\n 4-Veterinarian\n 5-");
     }
 }
+
+        }
+
+
