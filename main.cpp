de84344d1c018937d559215badf598dbefa154fb
ATeF66-eng <abdelrahmanelsabea@gmail.com>
2025-04-26 14:06:18 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -26,29 +26,51 @@ void job() {
     }
 }
 int main(void) {
-    printf("Welcome to Hirex\n the perfect place to find/offer a job\n");
+    printf("-------- Welcome to Hirex --------\n   (the best place to find & offer a job) \n");
     int x;
-    printf("choose\n 1-find a job\n 2-offer a job\n");
+    printf("select an option \n 1-find a job\n 2-offer a job\n");
     scanf("%d",&x);
-    if(x==1) {
-        printf("1-Engineering\n 2-Medical field\n 3-Science\n 4-Business\n 5-Education\n");
-        int field=0;
-        scanf("%d",&field);
-        switch(field) {
-            case 1:
 
-            printf("1-Programming Engineer | Full Time | Cairo | at Siemens digital industries \n2-Communication Engineer | Part Time | Giza | at EgyptAir  Company \n3-IT Engineer | Part Time | Remotely | at FAMA Company \n4-Architecture Engineer| Full Time | Dubai | at Arabian Conctruction Company \n5-Electric field | Internship | Cairo | at Elswedy Electric Company \n");
-
-          job();
-break;
-
-            case 2:
-                printf("1-Cardiologist | full time | Cairo |at Clinca Egypt \n 2-Dentist | part time | Giza |at Al-Arabi clinics  \n 3-Surgeon | internship | Mansoura |at Technoclinc specialized clinics\n 4-Veterinarian |part time| Zifta |at Al Safa Veterinary clinic  \n 5-Allergist |full time| Cairo| at Core clinic|\n");
-            job();
-            break;
-            case 3:
-                printf("1-Biologist | full time | Cairo | at downtown| 2-Chemist | full time | 6th of Octobr city | at Chemical industry| 3-")
-        }
-        }
+    while(x>2) {
+        printf("select an option \n 1-find a job\n 2-offer a job\n");
+        scanf("%d",&x);
     }
 
+        switch (x) {
+
+            case 1:
+
+                printf(" 1-Engineering\n 2-Medical field\n 3-Science\n 4-Business\n 5-Education\n");
+            int field;
+            scanf("%d",&field);
+            switch(field) {
+                case 1:
+
+                    printf("1-Programming Engineer | Full Time | Cairo | at Siemens digital industries \n2-Communication Engineer | Part Time | Giza | at EgyptAir  Company \n3-IT Engineer | Part Time | Remotely | at FAMA Company \n4-Architecture Engineer| Full Time | Dubai | at Arabian Conctruction Company \n5-Electric field | Internship | Cairo | at Elswedy Electric Company \n");
+
+                job();
+                break;
+
+                case 2:
+                    printf(" 1-Cardiologist | full time | Cairo |at Clinca Egypt \n 2-Dentist | part time | Giza |at Al-Arabi clinics  \n 3-Surgeon | internship | Mansoura |at Technoclinc specialized clinics\n 4-Veterinarian |part time| Zifta |at Al Safa Veterinary clinic  \n 5-Allergist |full time| Cairo| at Core clinic|\n");
+                job();
+                break;
+                case 3:
+                    printf("1-Biologist | full time | Cairo | at downtown| 2-Chemist | full time | 6th of October city | at Chemical industry| 3-Environmental scientist | full time | ");
+                break;
+
+
+            }
+break;
+            case 2:
+
+                printf("company");
+            break;
+            default:
+                printf("Error : Try again\n");
+            break;
+
+        }
+
+
+}
