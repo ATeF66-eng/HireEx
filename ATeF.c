437d457b538add588c912ab25b16f940bde02c39
ATeF66-eng <abdelrahmanelsabea@gmail.com>
2025-04-28 00:04:01 +0300

    Update ATeF.c

--- ATeF.c
+++ ATeF.c
@@ -15,7 +15,7 @@ void print() {
 void job() {
     int type,age;
     char name[100];
-    printf("Enter type:");
+    printf("Choose type:  ");
     scanf("%d",&type);
 
 
@@ -70,6 +70,15 @@ void job2() {
     printf("The requirements for this job are:\n 1-Bachelor of medicine \n 2-Have good English\n 3-Have experience\n Do you have these skills? \n answer yes or no: ");
     scanf("%s",&cv);
 }
+void job3() {
+    char cv[100];
+    printf("The requirements for this job are:\n 1-Bachelor of science \n 2-");
+}
+void job4() {
+    char cv[100];
+    printf("The requirements for this job are:\n 1-Bachelor of Education \n 2-Communication skill \n 3-Explanation skill \n 4- Encouragement skill \n Do you have these skills? \n answer yes or no ");
+scanf("%s",&cv);
+}
 int main(void) {
 
         int x;
@@ -98,31 +107,34 @@ int main(void) {
                     printe();
                     printf(" 1-Engineering\n 2-Medical field\n 3-Science\n 4-Education\n");
                     scanf("%d",&field);
-
                     }
 
                 switch(field) {
 
                     case 1:
-                        job();
+
                         job1();
                         printf("1-Programming Engineer | Full Time | Cairo | at Siemens digital industries \n2-Communication Engineer | Part Time | Giza | at EgyptAir  Company \n3-IT Engineer | Part Time | Remotely | at FAMA Company \n4-Architecture Engineer| Full Time | Dubai | at Arabian Conctruction Company \n5-Electric field | Internship | Cairo | at Elswedy Electric Company \n");
+                    job();
                     break;
 
                     case 2:
 
                         job2();
                         printf(" 1-Cardiologist | full time | Cairo |at Clinca Egypt \n 2-Dentist | part time | Giza |at Al-Arabi clinics  \n 3-Surgeon | internship | Mansoura |at Technoclinc specialized clinics\n 4-Veterinarian |part time| Zifta |at Al Safa Veterinary clinic  \n 5-Allergist |full time| Cairo| at Core clinic\n");
+                    job();
                     break;
 
                     case 3:
-
+job3();
                         printf("1-Biologist | full time | Cairo | at downtown \n 2-Chemist | full time | 6th of October city | at Chemical industry \n 3-Environmental scientist | full time |at Chemical industry\n 4-Data scientist | remotly | Cairo |at aptendo\n 5-Aeronautical Engineer | full time | Giza |at chemical industry \n");
+                    job();
                     break;
 
                     case 4:
-
+job4();
                         printf("1-Math teacher | full time | Cairo |at al-Ahram secondary school \n 2-Arabic teacher | part time | Mansoura | at al-shahid Ahmed El-Mansy school\n 3-school principal | full time | Mit Ghamr |at Mohamed Koraim primary school\n 4-English teacher | part time | Zagazig |at Omar makram secondary  school\n 5-Physics teacher | full time | Alexandria |at Soliman El-halaby secondary school\n ");
+                    job();
                     break;
                     }
             break;
