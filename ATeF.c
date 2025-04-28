c2288979c5acc21032cd7e079584f4323a8945c8
Abdoullah27 <138868377+Abdoullah27@users.noreply.github.com>
2025-04-29 00:32:10 +0300

    Update Atofa.c

--- ATeF.c
+++ ATeF.c
@@ -4,7 +4,7 @@
 #include <math.h>
 
 void printe() {
-    printf("Invalid Number... Try again\n\n");
+    printf("\n Invalid Number... Try again\n\n");
 }
 
 void print() {
@@ -13,6 +13,7 @@ void print() {
 }
 
 void job() {
+
     int type,age;
     char name[100];
     printf("Choose type:  ");
@@ -23,7 +24,7 @@ void job() {
         printe();
         printf("Enter number from 1 to 5:");
         scanf("%d",&type);
-}
+    }
 
     switch (type) {
         case 1:
@@ -31,150 +32,165 @@ void job() {
             scanf("%s",&name);
             printf("Enter your age: ");
             scanf("%d",&age);
-            break;
+        break;
+
         case 2:
             printf(" Enter your name: ");
-        scanf("%s",&name);
-        printf("Enter your age:");
-        scanf("%d",&age);
-            break;
+            scanf("%s",&name);
+            printf("Enter your age:");
+            scanf("%d",&age);
+        break;
+
         case 3:
             printf(" Enter your name: ");
-        scanf("%s",&name);
-        printf("Enter your age:");
-        scanf("%d",&age);
-            break;
+            scanf("%s",&name);
+            printf("Enter your age:");
+            scanf("%d",&age);
+        break;
+
         case 4:
             printf(" Enter your name: ");
-        scanf("%s",&name);
-        printf("Enter your age:");
-        scanf("%d",&age);
-            break;
+            scanf("%s",&name);
+            printf("Enter your age:");
+            scanf("%d",&age);
+        break;
+
         case 5:
             printf(" Enter your name: ");
-        scanf("%s",&name);
+            scanf("%s",&name);
 
-        printf("Enter your age:");
-        scanf("%d",&age);
+            printf(" Enter your age:");
+            scanf("%d",&age);
             break;
+
         default:
             printe();
     }
 }
+
+
+
 void job1() {
     char cv[100];
-    printf("\nThe requirements for this job are: \n");
-    printf("1-Bachelor of Engineering \n Do you have this skill? \n answer yes or no:");
+    printf("\n\n\nThe requirements for this job are: \n");
+    printf("\n 1-Bachelor of Engineering \n   Do you have this skill? \n\n Answer yes or no: ");
     scanf("%s",&cv);
-    printf("\n2-Have good English\n Do you have this skill? \n answer yes or no:");
+    printf("\n 2-Have good English\n   Do you have this skill? \n\n Answer yes or no: ");
     scanf("%s",&cv);
-    printf("\n3-Have experience\n Do you have this skill? \n answer yes or no:");
+    printf("\n 3-How many years of experience do u have");
     scanf("%s",&cv);
 }
+
 void job2() {
     char cv[100];
-    printf("\nThe requirements for this job are:\n");
-    printf("1-Bachelor of medicine \n  Do you have this skill? \n answer yes or no: ");
+    printf("\n\n\nThe requirements for this job are:\n");
+    printf(" 1-Bachelor of medicine \n   Do you have this skill? \n\n Answer yes or no: ");
     scanf("%s",&cv);
-    printf("\n2-Have good English\n Do you have this skill?\n answer yes or no: ");
+    printf("\n 2-Have good English\n   Do you have this skill?\n\n Answer yes or no: ");
     scanf("%s",&cv);
-    printf("\n3-Have experience\n Do you have this skill? \n answer yes or no: ");
-    scanf("%s",&cv);
+    printf("\n 3-How many years of experience do u have");
 }
+
 void job3() {
     char cv[100];
-    printf("\nThe requirements for this job are:\n ");
-    printf("1-Bachelor of science \n Do you have this skill? \n answer yes or no: ");
+    printf("\n\n\nThe requirements for this job are:\n ");
+    printf(" 1-Bachelor of science \n   Do you have this skill? \n\n Answer yes or no: ");
     scanf("%s",&cv);
-    ;printf("\n2-Practical skill \n Do you have this skill?\n answer yes or no: ");
+    ;printf("\n 2-Practical skill \n   Do you have this skill?\n\n Answer yes or no: ");
     scanf("%s",&cv);
-    printf("\n3-Soft skills \n Do you have this skill?\n answer yes or no: ");
+    printf("\n 3-Soft skills \n   Do you have this skill?\n\n Answer yes or no: ");
     scanf("%s",&cv);
-    printf("\n4-Good English \n Do you have this skill? \n answer yes or no: ");
-    scanf("%s",&cv);
+    printf("\n 4-Good English \n   Do you have this skill? \n\n Answer yes or no: ");
 }
+
 void job4() {
     char cv[100];
-    printf("\nThe requirements for this job are:\n ");
-    printf("1-Bachelor of Education\nDo you have this skill? \n answer yes or no: ");
+    printf("\n\n\nThe requirements for this job are:\n ");
+    printf(" 1-Bachelor of Education \n   Do you have this skill? \n\n Answer yes or no: ");
     scanf("%s",&cv);
-    printf("\n2-Communication skill\nDo you have this skill? \n answer yes or no: ");
+    printf("\n 2-Communication skill \n   Do you have this skill? \n\n Answer yes or no: ");
     scanf("%s",&cv);
-    printf("\n3-Explanation skill\nDo you have this skill? \n answer yes or no: ");
+    printf("\n 3-Explanation skill \n   Do you have this skill? \n\n Answer yes or no: ");
     scanf("%s",&cv);
-    printf("\n4- Encouragement skill\nDo you have this skill? \n answer yes or no: ");
+    printf("\n 4- Encouragement skill \n   Do you have this skill? \n\n Answer yes or no: ");
     scanf("%s",&cv);
 }
+
 int main(void) {
 
-        int x;
+    int x;
 
-        printf("-------- Welcome to Hirex --------\n   (the best place to find & offer a job) \n");
+    printf("-------- Welcome to Hirex --------\n   (the best place to find & offer a job) \n");
+    print();
+    scanf("%d",&x);
+
+    while(x!=1 && x!=2) {
+        printe();
         print();
         scanf("%d",&x);
-        while(x!=1 && x!=2) {
-            printe();
-            print();
-            scanf("%d",&x);
-        }
+    }
 
-        switch (x) {
+    switch (x) {
 
-            case 1:
+        case 1:
 
-                printf("\n1-Engineering\n 2-Medical field\n 3-Science\n 4-Education\n");
-
-                int field;
+            printf("\n 1-Engineering\n 2-Medical field\n 3-Science\n 4-Education\n");
+            int field;
             printf("Choose a field: ");
+            scanf("%d",&field);
+
+            while(field!=1  && field!=2 && field!=3 && field!=4) {
+                printe();                
+                printf(" 1-Engineering\n 2-Medical field\n 3-Science\n 4-Education\n");
+                printf("Choose a field: ");
                 scanf("%d",&field);
+                }
 
-                while(field!=1  && field!=2 && field!=3 && field!=4) {
-                    printe();
-                    printf(" 1-Engineering\n 2-Medical field\n 3-Science\n 4-Education\n");
-                    scanf("%d",&field);
-                    }
+            switch(field) {
 
-                switch(field) {
+                case 1:
 
-                    case 1:
+                    job1();
+                    printf("1-Programming Engineer | Full Time | Cairo | at Siemens digital industries \n2-Communication Engineer | Part Time | Giza | at EgyptAir  Company \n3-IT Engineer | Part Time | Remotely | at FAMA Company \n4-Architecture Engineer| Full Time | Dubai | at Arabian Conctruction Company \n5-Electric field | Internship | Cairo | at Elswedy Electric Company \n");
+                    job(); 
 
-                        job1();
-                        printf("1-Programming Engineer | Full Time | Cairo | at Siemens digital industries \n2-Communication Engineer | Part Time | Giza | at EgyptAir  Company \n3-IT Engineer | Part Time | Remotely | at FAMA Company \n4-Architecture Engineer| Full Time | Dubai | at Arabian Conctruction Company \n5-Electric field | Internship | Cairo | at Elswedy Electric Company \n");
+                break;
+
+                case 2:
+
+                    job2();
+                    printf(" 1-Cardiologist | full time | Cairo |at Clinca Egypt \n 2-Dentist | part time | Giza |at Al-Arabi clinics  \n 3-Surgeon | internship | Mansoura |at Technoclinc specialized clinics\n 4-Veterinarian |part time| Zifta |at Al Safa Veterinary clinic  \n 5-Allergist |full time| Cairo| at Core clinic\n");
                     job();
-                    break;
 
-                    case 2:
+                break;
 
-                        job2();
-                        printf(" 1-Cardiologist | full time | Cairo |at Clinca Egypt \n 2-Dentist | part time | Giza |at Al-Arabi clinics  \n 3-Surgeon | internship | Mansoura |at Technoclinc specialized clinics\n 4-Veterinarian |part time| Zifta |at Al Safa Veterinary clinic  \n 5-Allergist |full time| Cairo| at Core clinic\n");
+                case 3:
+                    job3();
+                    printf("1-Biologist | full time | Cairo | at downtown \n 2-Chemist | full time | 6th of October city | at Chemical industry \n 3-Environmental scientist | full time |at Chemical industry\n 4-Data scientist | remotly | Cairo |at aptendo\n 5-Aeronautical Engineer | full time | Giza |at chemical industry \n");
                     job();
-                    break;
 
-                    case 3:
-job3();
-                        printf("1-Biologist | full time | Cairo | at downtown \n 2-Chemist | full time | 6th of October city | at Chemical industry \n 3-Environmental scientist | full time |at Chemical industry\n 4-Data scientist | remotly | Cairo |at aptendo\n 5-Aeronautical Engineer | full time | Giza |at chemical industry \n");
+                break;
+
+                case 4:
+                    job4();
+                    printf("1-Math teacher | full time | Cairo |at al-Ahram secondary school \n 2-Arabic teacher | part time | Mansoura | at al-shahid Ahmed El-Mansy school\n 3-school principal | full time | Mit Ghamr |at Mohamed Koraim primary school\n 4-English teacher | part time | Zagazig |at Omar makram secondary  school\n 5-Physics teacher | full time | Alexandria |at Soliman El-halaby secondary school\n ");
                     job();
-                    break;
-
-                    case 4:
-job4();
-                        printf("1-Math teacher | full time | Cairo |at al-Ahram secondary school \n 2-Arabic teacher | part time | Mansoura | at al-shahid Ahmed El-Mansy school\n 3-school principal | full time | Mit Ghamr |at Mohamed Koraim primary school\n 4-English teacher | part time | Zagazig |at Omar makram secondary  school\n 5-Physics teacher | full time | Alexandria |at Soliman El-halaby secondary school\n ");
-                    job();
-                    break;
-                    }
-            break;
+                    
+                break;
+                }
+        break;
 
 
-            case 2:
-                printf("company");
-            break;
+        case 2:
+            printf("company");
+        break;
 
 
-            default:
-                printe();
-            break;
+        default:
+            printe();
+        break;
 
-        }
+    }
 
 
 }
