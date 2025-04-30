5bb0e825ef28f6828c4fe47bd99a4886dd4731dc
ATeF66-eng <abdelrahmanelsabea@gmail.com>
2025-04-30 12:28:51 +0300

    Update Atofa.c

--- Atofa.c
+++ Atofa.c
@@ -3,22 +3,27 @@
 #include <string.h>
 #include <math.h>
 
-void job() {
-
-    int type,age;
+int job() {
+    int type, age;
     char name[100];
+
     printf("\n Choose type:  ");
     scanf("%d", &type);
 
-
-    while(type !=1 && type !=2 && type !=3 && type !=4 && type !=5) {
-        printf("\n Invalid Number... Try again\n\n");
-        printf("Enter number from 1 to 5:");
-        scanf("%d", &type);
+    if (type == 6) {
+        return 0;
     }
 
+    while (type != 1 && type != 2 && type != 3 && type != 4 && type != 5 && type != 6) {
+        printf("\n Invalid Number... Try again\n\n");
+        printf("Enter number from 1 to 6:");
+        scanf("%d", &type);
+        if (type == 6) return 0;
+    }
 
+    return 1;
 }
+
 
 void req() {
 
@@ -97,7 +102,7 @@ void job4() {
 }
 
 void acceptance(){
-
+int exp;
     if(exp >= 3) {
         printf("\nThanks for your submit... \n Our experts are reviewing your form , we will call you back soon\n\n");
     }
@@ -113,12 +118,12 @@ int main(void) {
     int x;
 
     printf("-------- Welcome to Hirex --------\n   (the best place to find & offer a job) \n");
-    printf(" 1-find a job\n 2-offer a job \n select an option:");;
+    printf(" 1-find a job\n 2-offer a job \n 3-exit \n select an option:");;
     scanf("%d",&x);
 
-    while(x!=1 && x!=2) {
+    while(x!=1 && x!=2 && x!=3) {
         printf("\n Invalid Number... Try again\n\n");
-        printf(" 1-find a job\n 2-offer a job \n select an option:");;
+        printf(" 1-find a job\n 2-offer a job \n 3-exit \n select an option:");;
         scanf("%d", &x);
     }
 
@@ -126,14 +131,14 @@ int main(void) {
 
         case 1:
 
-            printf("\n 1-Engineering\n 2-Medical field\n 3-Science\n 4-Education\n");
+            printf("\n 1-Engineering\n 2-Medical field\n 3-Science\n 4-Education\n 5-exit \n");
             int field;
             printf("Choose a field:");
             scanf("%d",&field);
 
-            while(field!=1  && field!=2 && field!=3 && field!=4) {
-                printf("\n Invalid Number... Try again\n\n");                
-                printf(" 1-Engineering\n 2-Medical field\n 3-Science\n 4-Education\n");
+            while(field!=1  && field!=2 && field!=3 && field!=4 && field!=5 && field!=6) {
+                printf("\n Invalid Number... Try again\n\n");
+                printf(" 1-Engineering\n 2-Medical field\n 3-Science\n 4-Education\n 5-exit \n");
                 printf("Choose a field:");
                 scanf("%d", &field);
                 }
@@ -141,32 +146,34 @@ int main(void) {
             switch(field) {
 
                 case 1:
-                    printf("\n1-Programming Engineer | Full Time | Cairo | at Siemens digital industries \n2-Communication Engineer | Part Time | Giza | at EgyptAir  Company \n3-IT Engineer | Part Time | Remotely | at FAMA Company \n4-Architecture Engineer| Full Time | Dubai | at Arabian Conctruction Company \n5-Electric field | Internship | Cairo | at Elswedy Electric Company \n");
-                    job(); 
+                    printf("\n1-Programming Engineer | Full Time | Cairo | at Siemens digital industries \n2-Communication Engineer | Part Time | Giza | at EgyptAir  Company \n3-IT Engineer | Part Time | Remotely | at FAMA Company \n4-Architecture Engineer| Full Time | Dubai | at Arabian Conctruction Company \n5-Electric field | Internship | Cairo | at Elswedy Electric Company \n 6-exit\n");
+                if (job() == 0) return 0;
                     job1();
                     acceptance();
                 break;
 
                 case 2:
-                    printf(" 1-Cardiologist | full time | Cairo |at Clinca Egypt \n 2-Dentist | part time | Giza |at Al-Arabi clinics  \n 3-Surgeon | internship | Mansoura |at Technoclinc specialized clinics\n 4-Veterinarian |part time| Zifta |at Al Safa Veterinary clinic  \n 5-Allergist |full time| Cairo| at Core clinic\n");
-                    job();
+                    printf(" 1-Cardiologist | full time | Cairo |at Clinca Egypt \n 2-Dentist | part time | Giza |at Al-Arabi clinics  \n 3-Surgeon | internship | Mansoura |at Technoclinc specialized clinics\n 4-Veterinarian |part time| Zifta |at Al Safa Veterinary clinic  \n 5-Allergist |full time| Cairo| at Core clinic\n 6-exit\n");
+                if (job() == 0) return 0;
                     job2();
                     acceptance();
                 break;
 
                 case 3:
-                    printf("1-Biologist | full time | Cairo | at downtown \n 2-Chemist | full time | 6th of October city | at Chemical industry \n 3-Environmental scientist | full time |at Chemical industry\n 4-Data scientist | remotly | Cairo |at aptendo\n 5-Aeronautical Engineer | full time | Giza |at chemical industry \n");
-                    job();
+                    printf("1-Biologist | full time | Cairo | at downtown \n 2-Chemist | full time | 6th of October city | at Chemical industry \n 3-Environmental scientist | full time |at Chemical industry\n 4-Data scientist | remotly | Cairo |at aptendo\n 5-Aeronautical Engineer | full time | Giza |at chemical industry \n 6-exit\n");
+                if (job() == 0) return 0;
                     job3();
                     acceptance();
                 break;
 
-                case 4:                 
-                    printf("1-Math teacher | full time | Cairo |at al-Ahram secondary school \n 2-Arabic teacher | part time | Mansoura | at al-shahid Ahmed El-Mansy school\n 3-school principal | full time | Mit Ghamr |at Mohamed Koraim primary school\n 4-English teacher | part time | Zagazig |at Omar makram secondary  school\n 5-Physics teacher | full time | Alexandria |at Soliman El-halaby secondary school\n ");
-                    job();
+                case 4:
+                    printf("1-Math teacher | full time | Cairo |at al-Ahram secondary school \n 2-Arabic teacher | part time | Mansoura | at al-shahid Ahmed El-Mansy school\n 3-school principal | full time | Mit Ghamr |at Mohamed Koraim primary school\n 4-English teacher | part time | Zagazig |at Omar makram secondary  school\n 5-Physics teacher | full time | Alexandria |at Soliman El-halaby secondary school\n 6-exit\n");
+                if (job() == 0) return 0;
                     job4();
                     acceptance();
                 break;
+                case 5:
+                    return 0;
                 }
         break;
 
@@ -180,6 +187,8 @@ int main(void) {
             printf("\n Invalid Number... Try again\n\n");
         break;
 
+        case 3:
+            return 0;
     }
 
 
